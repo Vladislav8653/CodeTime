18ca105def1f89c38c24bfdb9ea839eccd5b8202
Vladislav8653 <arefin.vlad@gmail.com>
2024-06-02 21:58:03 +0300

    попытка 2

---
+++ Hubs/Authentication.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class Authentication : Hub
+    {
+        public async Task GetUserInfo(string username, string password)
+        {
+            string filePath = "D:/users.txt";
+            string field = "Login: " + username + " " + password;
+            await AppendTextToFileAsync(filePath, field);
+        }
+        
+        public async Task GetNewUserInfo(string username, string password)
+        {
+            string filePath = "D:/users.txt";
+            string field = "New user: " + username + " " + password;
+            await AppendTextToFileAsync(filePath, field);
+        }
+        
+        public async Task AppendTextToFileAsync(string filePath, string text)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                await writer.WriteLineAsync(text);
+            }
+        }
+    }
+}
