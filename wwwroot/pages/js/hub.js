9111bb032b18fad06903a0b9681f809e1e4dfdb8
Vladislav8653 <arefin.vlad@gmail.com>
2024-06-03 03:14:11 +0300

    немного ооп

---
+++ wwwroot/pages/js/hub.js
@@ -0,0 +1,16 @@
+﻿class Hub {
+    static instance = null;
+
+    constructor() {
+        if (Hub.instance) {
+            return Hub.instance;
+        }
+        this.connection = new signalR.HubConnectionBuilder().withUrl("/ClientLogic").build();
+        this.connection.start();
+        Hub.instance = this;
+    }
+
+    getConnection() {
+        return this.connection;
+    }
+}
